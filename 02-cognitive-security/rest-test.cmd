814930d3bbe51f59313bf44619ef52d61aba2f47
ARN14 <ar14@outlook.com>
2023-05-12 14:42:22 +0300

    Completed module 2: Cognitive services overview

--- 02-cognitive-security/rest-test.cmd
+++ 02-cognitive-security/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "<yourEndpoint>/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <yourKey>" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
+curl -X POST "https://associatetrainingservices.cognitiveservices.azure.com//text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 6d017c9e112a41eaa9d8b5686f2a8f7b" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
